ddcc20dda194153472674f7cb4792853079eb19b
Abhishek kumar <94746009+abhishek7112001@users.noreply.github.com>
2025-02-11 18:18:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require("./src/app");
 
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = 8000;
 app.listen(PORT, () => {
